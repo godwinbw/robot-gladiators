d526b16c739db6dab75c0c6a5a7d7d79ab4a6268
Bryan Godwin <godwin.bryan.w@gmail.com>
2021-03-29 00:10:48 -0500

    added high score to local storage

--- assets/js/game.js
+++ assets/js/game.js
@@ -254,6 +254,7 @@ var doFight = function (enemyInfo) {
     window.alert("LOSE : " + playerInfo.name + " has died!");
   } else if (enemyInfo.health <= 0) {
     window.alert("WIN : " + enemyInfo.name + " has died!");
+    playerInfo.money = playerInfo.money + 20;
   }
 
   console.log("----- fight with " + enemyInfo.name + " is OVER! -----");
@@ -376,22 +377,34 @@ var startGame = function () {
 };
 
 var endGame = function () {
-  //if the player is still alive, player wins!
-  if (playerInfo.health > 0) {
-    window.alert(
-      "Great job, you've survived the game! You now have a score of " +
-        playerInfo.money +
-        "."
+  window.alert("The game has now ended. Let's see how you did!");
+
+  // check localStorage for high score, if it's not there, use 0
+  var highScore = localStorage.getItem("highscore");
+  if (highScore === null) {
+    highScore = 0;
+  }
+  // if player has more money than the high score, player has new high score!
+  if (playerInfo.money > highScore) {
+    localStorage.setItem("highscore", playerInfo.money);
+    localStorage.setItem("name", playerInfo.name);
+
+    alert(
+      playerInfo.name + " now has the high score of " + playerInfo.money + "!"
     );
   } else {
-    window.alert("You've lost your robot in battle.");
+    alert(
+      playerInfo.name +
+        " did not beat the high score of " +
+        highScore +
+        ". Maybe next time!"
+    );
   }
 
   // ask player if they'd like to play again
   var playAgainConfirm = window.confirm("Would you like to play again?");
 
   if (playAgainConfirm) {
-    //restart the game
     startGame();
   } else {
     window.alert("Thank you for playing Robot Gladiators! Come back soon!");
